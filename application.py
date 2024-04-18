28369b57b9b9429b2a00621925773ae9a1d46181
Bhavish pothuraju <bhavishpothuraju123@gmail.com>
2024-04-18 11:48:32 +0530

    changes in application.py file

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request
 from main import main
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@application.route('/calculate_similarity', methods=['POST'])
+@app.route('/calculate_similarity', methods=['POST'])
 def calculate_similarity():
     if request.method == 'POST':
         text1 = request.form['text1']
@@ -17,6 +17,6 @@ def calculate_similarity():
         return render_template('index.html', similarity_score=similarity_score)
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    app.run(debug=True)
 
 
