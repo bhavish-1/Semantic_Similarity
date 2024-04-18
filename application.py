26daad52df48e9c7125d7d2d074001c6297755bf
Bhavish pothuraju <bhavishpothuraju123@gmail.com>
2024-04-18 12:05:37 +0530

    application

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request
 from main import main
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/calculate_similarity', methods=['POST'])
+@application.route('/calculate_similarity', methods=['POST'])
 def calculate_similarity():
     if request.method == 'POST':
         text1 = request.form['text1']
@@ -17,6 +17,6 @@ def calculate_similarity():
         return render_template('index.html', similarity_score=similarity_score)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
 
 
